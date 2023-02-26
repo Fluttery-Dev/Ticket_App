633ed81dd03d24caa77110b350982c56447e423d
Lokesh Mishra <flutterydev@gmail.com>
2023-02-26 15:01:35 +0530

    New Ticlet Project Created

---
+++ lib/Screens/bottom_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ticket App"),
+      ),
+    );
+  }
+}
