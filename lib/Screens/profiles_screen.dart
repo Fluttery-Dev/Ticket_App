ed74ee3912a25b61f466e3bfb8e25914ce0d617e
Lokesh Mishra <flutterydev@gmail.com>
2023-03-01 16:42:37 +0530

    To complete Profile Page

--- lib/Screens/profiles_screen.dart
+++ lib/Screens/profiles_screen.dart
@@ -1,4 +1,7 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_booking/util/app_layout.dart';
 import 'package:ticket_booking/util/app_styles.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -9,7 +12,105 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
-        children: [],
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: [
+          Gap(
+            AppLayout.getHeight(40),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: AppLayout.getHeight(90),
+                width: AppLayout.getWidth(90),
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(10)),
+                    image: const DecorationImage(
+                        image: AssetImage("assets/images/img_1.png"),
+                        fit: BoxFit.cover)),
+              ),
+              Gap(AppLayout.getWidth(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Book Tickets",
+                    style: Styles.headLineStyle1,
+                  ),
+                  Gap(AppLayout.getHeight(2)),
+                  Text(
+                    "New-York",
+                    style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14),
+                  ),
+                  Gap(AppLayout.getHeight(5)),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFFEF4F3),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(3),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF526799),
+                          ),
+                          child: const Icon(
+                            FluentSystemIcons.ic_fluent_shield_filled,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                        ),
+                        Gap(AppLayout.getWidth(5)),
+                        Text(
+                          "Premium Status",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: const Color(0xFF526799)),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Text(
+                "Edit",
+                style: Styles.textStyle.copyWith(color: Styles.primaryColor),
+              )
+            ],
+          ),
+          Gap(AppLayout.getHeight(20)),
+          Stack(
+            children: [
+              Container(
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Styles.primaryColor,
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              Positioned(
+                right: -45,
+                top: -45,
+                child: Container(
+                  padding: const EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 20,
+                      color: const Color(0xFF189999),
+                    ),
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
